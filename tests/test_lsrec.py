141724f66705febd84e39953745399ed7b876d5b
Bohan Zhang <32930283+AngelPone@users.noreply.github.com>
2025-12-02 19:50:27 +0800

    improve TESTCS

--- tests/test_lsrec.py
+++ tests/test_lsrec.py
@@ -1,105 +1,144 @@
-import numpy as np
-import forecopy as rpy
 import unittest
 
+import jax
+import jax.numpy as jnp
+import numpy as np
+from numpy.testing import assert_allclose, assert_array_equal
+
+import forecopy as rpy
+
+
 class TestCS(unittest.TestCase):
+    def setUp(self) -> None:
+        self.agg_mat = jnp.array([1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1]).reshape(3, 4)
+        self.cons_mat = jnp.hstack([jnp.eye(3, dtype="int32"), -self.agg_mat])
+        self.n = sum(self.agg_mat.shape)
+        self.m = self.agg_mat.shape[1]
+        self.rng = jax.random.key(123)
+        return super().setUp()
+
     def test_tools(self):
-        agg_mat = np.array([1,1,1,1,1,1,0,0,0,0,1,1]).reshape(3,4) 
-        cons_mat = np.array([1,0,0,-1,-1,-1,-1,0,1,0,-1,-1,0,0,0,0,1,0,0,-1,-1]).reshape(3,7)
-        tag = rpy.cstools(agg_mat=agg_mat)
-        tcm = rpy.cstools(cons_mat=cons_mat)
+        tag = rpy.cstools(agg_mat=self.agg_mat)
+        tcm = rpy.cstools(cons_mat=self.cons_mat)
 
-        with self.subTest():
-            self.assertTrue(np.all(tag.agg_mat==tcm.agg_mat))
-        with self.subTest():
-            self.assertTrue(np.all(tag.cons_mat()==tcm.cons_mat()))
-        with self.subTest():
-            self.assertTrue(np.all(tag.strc_mat()==tcm.strc_mat()))
-        with self.subTest():
-            self.assertTrue(np.all(tag.dim==tcm.dim))
-    
+        assert_array_equal(tag.agg_mat, tcm.agg_mat)
+        assert_array_equal(tag.cons_mat(), tcm.cons_mat())
+        assert_array_equal(tag.strc_mat(), tcm.strc_mat())
+        assert_array_equal(tag.dim, tcm.dim)
 
-    def test_solver_cov(self):
-        np.random.seed(123)
-
-        # Simulation parameters for base and residuals
-        h = 2    # Forecast horizons
+    def test_immutability(self):
+        h = 2  # Forecast horizons
         N = 100  # Number of residuals
-        agg_mat = np.array([1,1,1,1,1,1,0,0,0,0,1,1]).reshape(3,4) # Aggregation matrix
-        cons_mat = np.array([1,0,0,-1,-1,-1,-1,0,1,0,-1,-1,0,0,0,0,1,0,0,-1,-1]).reshape(3,7)
-        bts_mean = np.repeat(5, agg_mat.shape[1]) # Bottom time series' mean
-        mean = np.concatenate([agg_mat @ bts_mean, bts_mean]) # All time series' mean
+        bts_mean = jnp.repeat(5, self.agg_mat.shape[1])  # Bottom time series' mean
+        mean = jnp.concatenate(
+            [self.agg_mat @ bts_mean, bts_mean]
+        )  # All time series' mean
 
         # Simulated base forecasts
-        base = np.random.normal(
-            loc = np.concatenate([mean for i in range(h)]),
-            size = sum(agg_mat.shape)*h
-            ).reshape([h,sum(agg_mat.shape)])
+        base = jax.random.normal(self.rng, (h, self.n)) + mean
 
-        # Simulated residuals 
-        res = np.random.normal(
-            loc = np.concatenate([mean for i in range(N)]),
-            size = sum(agg_mat.shape)*N
-            ).reshape([N,sum(agg_mat.shape)]) - mean
-        
-        for i in ['ols', 'str', 'wls', 'shr', 'sam']:
+        # Simulated residuals
+        res = jax.random.normal(jax.random.split(self.rng)[0], (N, self.n))
+
+        for i in ["ols", "str", "wls", "shr", "sam"]:
+            with self.subTest(method=i):
+                r1 = rpy.csrec(
+                    base=base,
+                    res=res,
+                    comb=i,
+                    approach="proj",
+                    immutable=jnp.array([0, 2]),
+                    agg_mat=self.agg_mat,
+                )
+                r2 = rpy.csrec(
+                    base=base,
+                    res=res,
+                    comb=i,
+                    approach="proj_tol",
+                    immutable=jnp.array([0, 2]),
+                    agg_mat=self.agg_mat,
+                )
+                assert_allclose(r1, r2, rtol=1e-3)
+                assert_allclose(r1[:, [0, 2]], base[:, [0, 2]], rtol=1e-6)
+
+    def test_solver_cov(self):
+        # Simulation parameters for base and residuals
+        h = 2  # Forecast horizons
+        N = 100  # Number of residuals
+        bts_mean = jnp.repeat(5, self.agg_mat.shape[1])  # Bottom time series' mean
+        mean = jnp.concatenate(
+            [self.agg_mat @ bts_mean, bts_mean]
+        )  # All time series' mean
+
+        # Simulated base forecasts
+        base = jax.random.normal(self.rng, (h, self.n)) + mean
+
+        # Simulated residuals
+        res = jax.random.normal(jax.random.split(self.rng)[0], (N, self.n))
+
+        for i in ["ols", "str", "wls", "shr", "sam"]:
             r1 = rpy.csrec(
-                base=base, res=res, comb=i, approach='proj', agg_mat=agg_mat
-                )
+                base=base, res=res, comb=i, approach="proj", agg_mat=self.agg_mat
+            )
             r2 = rpy.csrec(
-                base=base, res=res, comb=i, approach='strc', agg_mat=agg_mat
-                )
+                base=base, res=res, comb=i, approach="strc", agg_mat=self.agg_mat
+            )
             r3 = rpy.csrec(
-                base=base, res=res, comb=i, approach='proj', cons_mat=cons_mat
-                )
+                base=base, res=res, comb=i, approach="proj", cons_mat=self.cons_mat
+            )
             r4 = rpy.csrec(
-                base=base, res=res, comb=i, approach='strc', cons_mat=cons_mat
-                )
-            
+                base=base, res=res, comb=i, approach="strc", cons_mat=self.cons_mat
+            )
+
             r5 = rpy.csrec(
-                base=base, res=res, comb=i, approach='proj', 
-                cons_mat=cons_mat, solver='linearx'
-                )
+                base=base,
+                res=res,
+                comb=i,
+                approach="proj",
+                cons_mat=self.cons_mat,
+                solver="linearx",
+            )
             r6 = rpy.csrec(
-                base=base, res=res, comb=i, approach='strc', 
-                cons_mat=cons_mat, solver='linearx'
-                )
+                base=base,
+                res=res,
+                comb=i,
+                approach="strc",
+                cons_mat=self.cons_mat,
+                solver="linearx",
+            )
 
             r7 = rpy.csrec(
-                base=base, res=res, comb=i, approach='proj_tol', 
-                cons_mat=cons_mat
-                )
+                base=base, res=res, comb=i, approach="proj_tol", cons_mat=self.cons_mat
+            )
             r8 = rpy.csrec(
-                base=base, res=res, comb=i, approach='strc_tol', 
-                cons_mat=cons_mat
-                )
+                base=base, res=res, comb=i, approach="strc_tol", cons_mat=self.cons_mat
+            )
             r9 = rpy.csrec(
-                base=base, res=res, comb=i, approach='proj_tol', 
-                cons_mat=cons_mat, solver='linearx'
-                )
+                base=base,
+                res=res,
+                comb=i,
+                approach="proj_tol",
+                cons_mat=self.cons_mat,
+                solver="linearx",
+            )
             r10 = rpy.csrec(
-                base=base, res=res, comb=i, approach='strc_tol', 
-                cons_mat=cons_mat, solver='linearx'
-                )
+                base=base,
+                res=res,
+                comb=i,
+                approach="strc_tol",
+                cons_mat=self.cons_mat,
+                solver="linearx",
+            )
 
-            with self.subTest():
-                self.assertTrue(np.mean((r1-r2)**2)<1e-6)
-            with self.subTest():
-                self.assertTrue(np.mean((r1-r3)**2)<1e-6)
-            with self.subTest():
-                self.assertTrue(np.mean((r1-r4)**2)<1e-6)
-            with self.subTest():
-                self.assertTrue(np.mean((r1-r5)**2)<1e-6)
-            with self.subTest():
-                self.assertTrue(np.mean((r1-r6)**2)<1e-6)
-            with self.subTest():
-                self.assertTrue(np.mean((r1-r7)**2)<1e-6)
-            with self.subTest():
-                self.assertTrue(np.mean((r1-r8)**2)<1e-6)
-            with self.subTest():
-                self.assertTrue(np.mean((r1-r9)**2)<1e-6)
-            with self.subTest():
-                self.assertTrue(np.mean((r1-r10)**2)<1e-6)
+            assert_allclose(r1, r2, rtol=1e-3)
+            assert_allclose(r1, r3, rtol=1e-3)
+            assert_allclose(r1, r4, rtol=1e-3)
+            assert_allclose(r1, r5, rtol=1e-3)
+            assert_allclose(r1, r6, rtol=1e-3)
+            assert_allclose(r1, r7, rtol=1e-3)
+            assert_allclose(r1, r8, rtol=1e-3)
+            assert_allclose(r1, r9, rtol=1e-3)
+            assert_allclose(r1, r10, rtol=1e-3)
 
 
 class TestTE(unittest.TestCase):
